807f60afada1edaa841b8e0b11f4054c9a05cf41
aldairbalanzar <abalanzar7@gmail.com>
2020-09-05 02:45:37 -0400

    loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { motion, AnimateSharedLayout, AnimatePresence } from 'framer-motion';
+import LoaderAnimation from './LoaderAnimation';
+
+const Loader = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const handleLoading = () => {
+        setIsLoading(!isLoading)
+    }
+    return (
+        <div
+        style={{  
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
+            background: 'lightgrey',
+            color: 'charcoal',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column', }}>
+            <AnimateSharedLayout>
+                <motion.header layoutId='header' onClick={handleLoading}>
+                    <motion.h1 layoutId='logo'>Loader Component</motion.h1>
+                </motion.header>
+                <AnimatePresence>
+                {isLoading &&
+                   <LoaderAnimation handleLoading={handleLoading}/>
+                }
+                </AnimatePresence>
+            </AnimateSharedLayout>
+        </div>
+
+    )
+}
+
+export default Loader
